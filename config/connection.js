1b88aa4f240645869596125eed407803aa51c7c6
Brain <ortiz_brian323@yahoo.com>
2022-05-25 12:30:17 -0700

    passed through user crendetials from .env file

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    //imported from .env file
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
 }
 //exporting
